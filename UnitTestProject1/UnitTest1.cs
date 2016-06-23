650f8595d9e87b6ced5c1316ef686bf74b78be89
loveskyo <loveskyo@sina.com>
2016-06-23 14:42:12 +0800

    删除dbproj文件

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,24 +1,61 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
 
-
+    [DeploymentItem(@"add.xml")]
     [TestClass]
     public class UnitTest1
     {
+        List<testdata> list = new List<testdata>();
+
+        public TestContext TestContext { get; set; }
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            //string id = Convert.ToString(TestContext.DataRow["id"]);
+            // string name = Convert.ToString(TestContext.DataRow["name"]);
+            //string type = Convert.ToString(TestContext.DataRow["type"]);
+
+
+            //list.Add(new testdata { id = int.Parse(id), name = name, type = type });
+
+
+
+        }
+
+
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"|DataDirectory|\add.xml", "row", DataAccessMethod.Sequential)]
         public void TestMethod1()
         {
             var mock = new Mock<IFoo>();
             mock.Setup(foo => foo.DoSomething("ping")).Returns(true);
 
+            string id = Convert.ToString(TestContext.DataRow["id"]);
+            string name = Convert.ToString(TestContext.DataRow["name"]);
+            string type = Convert.ToString(TestContext.DataRow["type"]);
 
             Assert.AreEqual(true, mock.Object.DoSomething("ping"));
 
 
+            // out arguments
+            var outString = "ack";
+            // TryParse will return true, and the out argument will return "ack", lazy evaluated
+            mock.Setup(foo => foo.TryParse("ping", out outString)).Returns(true);
+
+            Assert.AreEqual(true, mock.Object.DoSomething("ping"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var mock = new Mock<IFoo>();
             // out arguments
             var outString = "ack";
             // TryParse will return true, and the out argument will return "ack", lazy evaluated
@@ -34,6 +71,12 @@ namespace UnitTestProject1
         bool TryParse(string s, out string v);
     }
 
+    public class testdata
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+    }
 
 
 }
